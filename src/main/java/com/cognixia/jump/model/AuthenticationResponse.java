bc7a8eba8d391d23dc34d95f40d44771cad59c38
Dyllan <dyllanmartino@gmail.com>
2022-05-18 13:39:27 -0500

    Added base security functionality to work with basic springboot framework

--- src/main/java/com/cognixia/jump/model/AuthenticationResponse.java
+++ src/main/java/com/cognixia/jump/model/AuthenticationResponse.java
@@ -1,2 +1,16 @@
-package com.cognixia.jump.model;public class AuthenticationResponse {
+package com.cognixia.jump.model;
+
+public class AuthenticationResponse {
+
+	private final String JWT;
+	
+	public AuthenticationResponse(String JWT) {
+		this.JWT = JWT;
+	}
+	
+	public String getJWT() {
+		return JWT;
+	}
+	
+	
 }
